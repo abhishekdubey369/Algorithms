c240d6cf78342e0490d9b44e1b2831c4f3f85e4c
abhishkedubey369 <abhigns369@gmail.com>
2024-11-06 01:22:13 +0530

    pusing incomplete mergeSort

---
+++ mergeSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class mergeSort {
+
+  private:
+    int* Arr;         // Pointer to the array
+    int size;         // size of an Array
+    int capacity;     // Dynamicness of Array
+
+  public:
+    mergeSort(int* A)
+  
+}
